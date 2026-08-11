f562d347aab0d7a9b306c1f6bcfd001881674c0c
Vaibhav Jangir <vaibhavjangir@Vaibhavs-MacBook-Air.local>
2026-08-11 19:48:23 +0530

    fix the cors setup

--- src/main/java/com/bluewave/civicconnect/config/CorsConfig.java
+++ src/main/java/com/bluewave/civicconnect/config/CorsConfig.java
@@ -15,9 +15,9 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5500", "http://127.0.0.1:5500","https://civicconnect-ui-lime.vercel.app"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT","PATCH", "DELETE"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5500", "http://127.0.0.1:5500", "https://civicconnect-ui-lime.vercel.app"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
